cdd952ff03fa6904ef6e0020c0f1e71d68476829
Kimberlee Ashmore <kimashmore13@gmail.com>
2023-02-10 11:15:29 -0600

    post class changes

--- django_twitter/twitter/models.py
+++ django_twitter/twitter/models.py
@@ -3,5 +3,6 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
 	title = models.CharField(max_length=120)
-	content = models.TextFeild()
-	date_posted = models.DateField()
+	content = models.TextField()
+	date_posted = models.DateField(auto_now_add=True)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
